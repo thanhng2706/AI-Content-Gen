da8d9331339929b72fc603152c5bc2fd27261a0e
ThanhNguyen <vnguyen1@wpi.edu>
2025-01-28 01:43:29 -0500

    Last Commit

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React, { useContext, useState } from 'react';
-import FormSection from '../_components/FormSection';
-import OutputSection from '../_components/OutputSection';
-import { TEMPLATE } from '../../_components/TemplateListSection';
-import Templates from '@/app/(data)/Templates';
+import React, { useContext, useState } from 'react'
+import FormSection from '../_components/FormSection'
+import OutputSection from '../_components/OutputSection'
+import { TEMPLATE } from '../../_components/TemplateListSection'
+import Templates from '@/app/(data)/Templates'
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
@@ -15,14 +15,15 @@ import moment from 'moment';
 import { TotalUsageContext } from '@/app/(context)/TotalUsageContext';
 import { useRouter } from 'next/navigation';
 
-type PageProps = {
+interface PROPS {
     params: {
         'template-slug': string
     }
 }
 
-export default function CreateNewContent({ params }: PageProps) {
-    const selectedTemplate: TEMPLATE | undefined = Templates?.find((item) => item.slug === params['template-slug']);
+function CreateNewContent(props: PROPS) {
+
+    const selectedTemplate: TEMPLATE | undefined = Templates?.find((item) => item.slug === props.params['template-slug']);
     const [loading, setLoading] = useState(false);
     const [aiOutput, setAiOuput] = useState<string>('');
     const { user } = useUser();
@@ -40,9 +41,10 @@ export default function CreateNewContent({ params }: PageProps) {
         setAiOuput(result?.response.text());
         await SaveInDb(JSON.stringify(formData), selectedTemplate?.slug, result?.response.text());
         setLoading(false);
-    };
+    }
 
     const SaveInDb = async (formData: any, slug: any, aiResp: string) => {
+
         const result = await db.insert(AIOutput).values({
             formData: formData,
             templateSlug: slug,
@@ -51,25 +53,25 @@ export default function CreateNewContent({ params }: PageProps) {
             createdAt: moment().format('YYYY-MM-DD HH:mm:ss')
         });
         console.log(result);
-    };
 
+    }
     return (
         <div className='p-5'>
             <Link href={"/dashboard"}>
-                <Button><ArrowLeft />Back</Button>
+                <Button ><ArrowLeft />Back</Button>
             </Link>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
                 {/* FormSection */}
-                <FormSection 
-                    selectedTemplate={selectedTemplate}
+                <FormSection selectedTemplate={selectedTemplate}
                     userFormInput={(v: any) => GenerateAIContent(v)}
-                    loading={loading} 
-                />
+                    loading={loading} />
                 {/* OutputSection */}
                 <div className='col-span-2'>
                     <OutputSection aiOutput={aiOutput} />
                 </div>
             </div>
         </div>
-    );
+    )
 }
+
+export default CreateNewContent
